8d1776713fd65d5a48223c05be40d20f59b09ab7
loganraff <122321572+loganraff@users.noreply.github.com>
2024-01-29 14:20:37 -0700

    Created general layout

--- dmit_2504_assignment01/lib/main.dart
+++ dmit_2504_assignment01/lib/main.dart
@@ -25,12 +25,83 @@ class MyApp extends StatelessWidget {
           title: const Text("App1 - UI Layout"),
           backgroundColor: Colors.blue,
         ),
-        body: const Row(
+        body: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           mainAxisSize: MainAxisSize.max,
           children: <Widget>[
             //TODO: Put your code here to complete this app.
-            
+            Column(
+              children: [
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.orange,
+                  child: const Center(
+                    child: Text(
+                      'Container 1'
+                    ),
+                  )
+                ),
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.white,
+                  child: const Center(
+                    child: Text(
+                      "Container 2"
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Column(
+              children: [
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.yellow,
+                  child: const Center(
+                    child: Text(
+                      "Container 3"
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.blue,
+                  child: const Center(
+                    child: Text(
+                      "Container 4"
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Column(
+              children: [
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.black,
+                  child: const Center(
+                    child: Text(
+                      "Container 5"
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.red,
+                  child: const Center(
+                    child: Text(
+                      "Con 6"
+                    ),
+                  ),
+                )
+              ],
+            )
           ],
         ),
       ),
